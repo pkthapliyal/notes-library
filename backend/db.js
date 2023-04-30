7cc688a4672f6c7cb428f9355abfd5def5141eaa
Pankaj Kumar <121335947+pkthapliyal@users.noreply.github.com>
2023-04-30 21:41:13 +0530

    Delete backend directory

--- backend/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-require('dotenv').config();
-const mongoURL = process.env.mongoURL;
-const connection = mongoose.connect(mongoURL);
-
-
-module.exports = { connection }
